2e4c05f46613a313975f21e7bba1d2b3751fa8b3
NICKY B <Nickblackburn02@gmail.com>
2018-06-05 11:04:34 -0400

    added cli menu

--- src/com/nicholasblackburn/Main.java
+++ src/com/nicholasblackburn/Main.java
@@ -5,9 +5,42 @@
 package com.nicholasblackburn;
 
 
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args){
+        int menu;
+
+        int selection;
+        Scanner input = new Scanner(System.in);
+
+        /***************************************************/
+
+        System.out.println("Nicks Password Generator");
+        System.out.println("-------------------------\n");
+        System.out.println("1 - rsa public and private key ");
+        System.out.println("2 - random pass");
+        System.out.println("3 - guide");
+        System.out.println("4 - Quit");
+        System.out.println("-------------------------\n");
+        selection = input.nextInt();
+        if (selection == 1){
+            keygenerator.main(args);
+        }
+        if (selection == 2){
+            pass.main(args);
+        }
+        if(selection ==3){
+            System.out.print("generates passwords \n");
+            System.out.print("generates rsa public and private keys ");
+        }
+        if (selection == 4){
+            System.exit(0);
+        }
+
+
+
     }
 }
+
